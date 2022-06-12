4b2f504a52ca8470683d3080ed0ddf4e4cfb59d4
Artur L <falcon.arthur@gmail.com>
2022-06-12 12:12:17 +0300

    EJS

--- app.js
+++ app.js
@@ -1,21 +1,49 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+
+
 const app = express();
+app.set('view engine', 'ejs');
 
-app.get("/", function(req, res){
+
+app.get("/", function (req, res) {
     var today = new Date();
-    if (today.getDay() === 6 || today.getDay() === 0) {
-        res.send("today is weekend " + today.getDay());
-    } else {
-        res.sendFile(__dirname + "/index.html");
-    }
+    var currentDay = today.getDay();
+    var day = "";
 
-    
-    
-    
+    switch (currentDay) {
+        case 0:
+            day = "Sunday"
+            break;
+        case 1:
+            day = "Monday"
+            break;
+        case 2:
+            day = "Tuesday"
+            break;
+        case 3:
+            day = "Wednesday"
+            break;
+        case 4:
+            day = "Thursday"
+            break;
+        case 5:
+            day = "Friday"
+            break;
+        case 6:
+            day = "Saturday"
+            break;
+        default:
+            console.log("ERROR");
+            break;
+    }
+    res.render("list", { day: day });
+
+
+
 });
 
-app.listen(3000, function(){
-  console.log("Server started on port 3000.");
+app.listen(3000, function () {
+    console.log("Server started on port 3000.");
 });
