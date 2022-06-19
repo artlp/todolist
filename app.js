861ab7ad543956ae3e19e55657fb9a2e83d0bf91
Artur L <falcon.arthur@gmail.com>
2022-06-19 18:29:56 +0300

    sad

--- app.js
+++ app.js
@@ -1,48 +1,29 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { use } = require("express/lib/application");
 
 
 
 const app = express();
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get("/", function (req, res) {
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
-
-    switch (currentDay) {
-        case 0:
-            day = "Sunday"
-            break;
-        case 1:
-            day = "Monday"
-            break;
-        case 2:
-            day = "Tuesday"
-            break;
-        case 3:
-            day = "Wednesday"
-            break;
-        case 4:
-            day = "Thursday"
-            break;
-        case 5:
-            day = "Friday"
-            break;
-        case 6:
-            day = "Saturday"
-            break;
-        default:
-            console.log("ERROR");
-            break;
-    }
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+    var day = today.toLocaleDateString("en-US", options);
+    console.log(today.month);
     res.render("list", { day: day });
-
-
-
 });
+
+app.post("/", function (req, res) {
+    let item = req.body.newItem;
+})
 
 app.listen(3000, function () {
     console.log("Server started on port 3000.");
